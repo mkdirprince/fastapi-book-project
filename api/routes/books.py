63064c6eaa8803dcbfaf36b630b2a11416431433
mkdirprince <mkdirprince@github.com>
2025-02-14 16:16:42 +0000

    correct status code for 200

--- api/routes/books.py
+++ api/routes/books.py
@@ -49,7 +49,7 @@ async def get_books() -> OrderedDict[int, Book]:
     return db.get_books()
    
    
-@router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_Ok)
+@router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
 async def get_book(book_id: int):
  book = db.get_book(book_id)
  if not book:
