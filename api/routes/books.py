2b94effff4cc98c3221915ff1ca30a48a378c1aa
mkdirprince <mkdirprince@github.com>
2025-02-14 16:18:39 +0000

    add return statement

--- api/routes/books.py
+++ api/routes/books.py
@@ -57,6 +57,7 @@ async def get_book(book_id: int):
    status_code = status.HTTP_404_NOT_FOUND,
    detail = "Book not found"
    )
+ return book
 
 
 @router.put("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
